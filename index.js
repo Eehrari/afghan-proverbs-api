039baa0f0c242b2c989bab5036f411602c613537
Ehsan Ehrari <ehsan.ehrari@codetoinspire.org>
2025-05-19 09:54:39 +0430

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 import express from 'express';
 import cors from 'cors';
 import proverbsRoutes from './routes/proverbs.js';
-
+const allowedOrigins = ['https://afghan-proverbs-ui-project.onrender.com','http://localhost:4200'];
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(cors({
+  origin: allowedOrigins
+}));
+
 app.use('/api/proverbs', proverbsRoutes);
 
 app.listen(PORT, () => {
